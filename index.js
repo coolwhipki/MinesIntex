decc5250988892f95d1acdd597ad6efe3fb650d3
coolwhipki <kirajanegirl@gmail.com>
2023-12-06 09:45:26 -0700

    showing lain what stage means then I deleted it

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname + '/landing.html'));
 });
 
+
 // random route to Login page
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
